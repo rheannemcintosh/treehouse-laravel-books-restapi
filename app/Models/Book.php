2ad44461213c96148dfe597ab17e71760aded226
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-17 23:13:12 +0000

    Update the books model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $table = 'books';
+
+    protected $fillable = [
+        'title',
+        'author_id',
+        'abstract'
+    ];
+
+    public function author() {
+        return $this->belongsTo(Author::class);
+    }
 }
